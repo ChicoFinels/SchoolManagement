c9a8bd256207f54ac42ca232e69ce7ba339d24e8
Francisco Fernandes <francisco.bonitos@gmail.com>
2024-12-23 18:11:39 +0000

    Finished customizing views and elements

---
+++ SchoolManagement/Models/ClassMetadata.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SchoolManagement.Data;
+
+public class ClassMetadata
+{
+    [Display(Name = "Lecturer Id")]
+    public int? LecturerId { get; set; }
+
+    [Display(Name = "Course Id")]
+    public int? CourseId { get; set; }
+
+    public TimeOnly? Time { get; set; }
+}
+
+[ModelMetadataType(typeof(ClassMetadata))]
+public partial class Class { }
---
+++ SchoolManagement/Models/EnrollmentMetadata.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SchoolManagement.Data;
+
+public class EnrollmentMetadata
+{
+    [Display(Name = "Student Id")]
+    public int? StudentId { get; set; }
+
+    [Display(Name = "Class Id")]
+    public int? ClassId { get; set; }
+
+    public string? Grade { get; set; }
+}
+
+[ModelMetadataType(typeof(EnrollmentMetadata))]
+public partial class Enrollment { }
